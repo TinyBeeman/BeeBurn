13982ce6cf1c798d79c9e74841e8d6d125dfd15c
Tony Beeman <beeman@gmail.com>
2020-05-08 22:55:52 -0700

    Display Selection

--- BeeBurn/MainWindow.xaml.cs
+++ BeeBurn/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -18,16 +19,11 @@ using System.Windows.Shapes;
 namespace BeeBurn
 {
 
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public class BeeBurnVM : INotifyPropertyChanged
     {
-        private Projection m_proj = null;
         private int m_pasteCounter = 0;
-
         private ObservableCollection<BeeImage> m_activeImages;
-        
+
         public ObservableCollection<BeeImage> ActiveImages
         {
             get => m_activeImages;
@@ -36,10 +32,22 @@ namespace BeeBurn
 
         private int m_activeSelectionIndex;
 
-        private int ActiveSelectionIndex
+        public int ActiveSelectionIndex
         {
             get => m_activeSelectionIndex;
-            set { m_activeSelectionIndex = value; OnPropertyChanged(); }
+            set
+            {
+                m_activeSelectionIndex = value; 
+                OnPropertyChanged();
+                ImageSourceToDisplay = m_activeImages[value].ImageSource;
+            }
+        }
+
+        private ImageSource m_imageSourceToDisplay;
+        public ImageSource ImageSourceToDisplay
+        {
+            get => m_imageSourceToDisplay;
+            set { m_imageSourceToDisplay = value; OnPropertyChanged(); }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -49,9 +57,34 @@ namespace BeeBurn
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
 
+        public void PasteToList()
+        {
+            if (ActiveImages == null)
+            {
+                ActiveImages = new ObservableCollection<BeeImage>();
+            }
+
+            ActiveImages.Add(new BeeImage(BeeClipboard.ImageFromClipboardDib(), "Paste-" + m_pasteCounter.ToString("D" + 4)));
+            m_pasteCounter++;
+        }
+
+    }
+
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Projection m_proj = null;
+        private BeeBurnVM m_VM = null;
+
+
         public MainWindow()
         {
             InitializeComponent();
+            m_VM = new BeeBurnVM();
+            DataContext = m_VM;
         }
 
         private void ClickProject(object sender, RoutedEventArgs e)
@@ -63,15 +96,7 @@ namespace BeeBurn
 
         private void ClickPaste(object sender, RoutedEventArgs e)
         {
-            if (ActiveImages == null)
-            {
-                ActiveImages = new ObservableCollection<BeeImage>();
-            }
-
-            ActiveImages.Add(new BeeImage(BeeClipboard.ImageFromClipboardDib(), "Paste-" + m_pasteCounter.ToString("D" + 4)));
-            m_pasteCounter++;
-            if (m_proj != null)
-                m_proj.PasteImage();
+            m_VM.PasteToList();
         }
     }
 }
